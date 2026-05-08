67a9069fd4a8cd917263e30ec4f339cd1a1800a2
manik <maniksarker652@gmail.com>
2026-05-08 20:51:40 +0600

    add an employee

--- restaurant-management/users.py
+++ restaurant-management/users.py
@@ -27,14 +27,17 @@ class Admin(User):
         super().__init__(name, phone, email, address)
         self.employees = [] # in memory database
 
-    def add_empoloyee(self,name,email,phone,address):
-        employee = Employee(name,email,phone,address) # employee class object
+    def add_empoloyee(self,name,email,phone,address,age,designation,salary):
+        employee = Employee(name,email,phone,address,age,designation,salary) # employee class object
         self.employees.append(employee)
         print(f'{name} is added!!')
 
     def view_employee(self):
-        print("Employee list:\n")
+        print("Employee list:")
         for emp in self.employees:
             print(emp.name, emp.email, emp.phone, emp.address)
 
+ad = Admin("Admin","admin@gmail.com","93849384","Dhaka")
+ad.add_empoloyee("Sagor","sagor@gmail.com","9834983","Khulna",34,"Chef",12000)
 
+ad.view_employee()
