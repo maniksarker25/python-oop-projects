0ea9063a1ccf3e341a45acf38e6e3346ab9cff77
manik <maniksarker652@gmail.com>
2026-05-08 21:05:37 +0600

    wrok with item menun , add item , remove item etc

--- restaurant-management/users.py
+++ restaurant-management/users.py
@@ -25,17 +25,49 @@ class Employee(User):
 class Admin(User):
     def __init__(self, name, email,phone, address):
         super().__init__(name, phone, email, address)
+
+    def add_empoloyee(self,restaurant,employee):
+        restaurant.add_employee(employee)
+
+    def view_employee(self,restaurant):
+        restaurant.view_employee
+
+
+class Restaurant:
+    def __init__(self,name):
+        self.name = name
         self.employees = [] # in memory database
 
-    def add_empoloyee(self,name,email,phone,address,age,designation,salary):
-        employee = Employee(name,email,phone,address,age,designation,salary) # employee class object
+    def add_empoloyee(self,employee):
         self.employees.append(employee)
-        print(f'{name} is added!!')
+        print(f'{employee.name} is added!!')
 
     def view_employee(self):
         print("Employee list:")
         for emp in self.employees:
             print(emp.name, emp.email, emp.phone, emp.address)
+
+class Menu:
+    def __init__(self):
+        self.items = [] # items ar database
+
+    def add_menu_item(self,item):
+        self.items.append(item)
+
+    def find_item(self,item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+            return None
+    
+    def remove_item(self,item_name):
+        item = self.find_item(item_name)
+        if item:
+            self.items.remove(item)
+            print(f"${item_name} deleted successfully from item menu!!")
+        else:
+            print("Item not found")
+
 
 ad = Admin("Admin","admin@gmail.com","93849384","Dhaka")
 ad.add_empoloyee("Sagor","sagor@gmail.com","9834983","Khulna",34,"Chef",12000)
