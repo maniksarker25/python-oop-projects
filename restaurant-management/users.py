a5c290b14db2c4662b91ac4dca0d3b07ebea3182
manik <maniksarker652@gmail.com>
2026-05-08 20:33:39 +0600

    create user and employee class

---
+++ restaurant-management/users.py
@@ -0,0 +1,20 @@
+# Customer
+# Employee
+# Admin
+
+from abc import ABC
+
+class User(ABC):
+    def __init__(self,name,phone,email,address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+     
+class Employee(User):
+    def __init__(self, name, phone, email, address,age,designation,salary):
+        super().__init__(name, phone, email, address)
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+        
