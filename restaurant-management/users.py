658870f5eca5ebe3f280e6e4815cc23268d7f5f6
manik <maniksarker652@gmail.com>
2026-05-08 20:44:06 +0600

    create admin class

--- restaurant-management/users.py
+++ restaurant-management/users.py
@@ -5,16 +5,29 @@
 from abc import ABC
 
 class User(ABC):
-    def __init__(self,name,phone,email,address):
+    def __init__(self,name,email,phone,address):
         self.name = name
         self.phone = phone
         self.email = email
         self.address = address
      
 class Employee(User):
-    def __init__(self, name, phone, email, address,age,designation,salary):
+    def __init__(self, name, email,phone, address,age,designation,salary):
         super().__init__(name, phone, email, address)
         self.age = age
         self.designation = designation
         self.salary = salary
-        
+
+
+# emp = Employee("rahim","rahim@gmail.com",932849234,"dhaka",23,"Chef",12000)
+# print(emp.name)
+
+class Admin(User):
+    def __init__(self, name, email,phone, address):
+        super().__init__(name, phone, email, address)
+        self.employees = [] # in memory database
+
+    def add_empoloyee(self,name,email,phone,address):
+        employee = Employee(name,email,phone,address) # employee class object
+        self.employees.append(employee)
+        print(f'{name} is added!!')
