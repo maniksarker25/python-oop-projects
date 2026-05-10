ad9e6b2b47ccc9454c62d7340bfaf38d863e9373
manik <maniksarker652@gmail.com>
2026-05-10 23:07:58 +0600

    work with student grade

--- school-management/person.py
+++ school-management/person.py
@@ -1,4 +1,5 @@
 import random
+from school import School
 class Person:
     def __init__(self,name):
         self.name = name
@@ -13,6 +14,27 @@ class Teacher(Person):
 
 
 class Student(Person):
-    def __init__(self, name):
+    def __init__(self, name,classroom):
         super().__init__(name)
+        self.classroom = classroom
+        self__id = None
+        self.marks = {} # {english:78,"bangla":90}
+        self.subject_grade = {} # {english:"A"}
+        self.grade = None # final grade
+
+    
+    def final_grade(self):
+        sum = 0
+        for grade in self.subject_grade.values():
+            point = School.grade_to_value(grade) # 5.00
+            sum += point
+        gpa = sum / len(self.subject_grade) # 7/2 = 3.50
+        self.grade = School.value_to_grade(gpa)
         
+    @property
+    def id(self):
+        return self.__id 
+        
+    @id.setter
+    def id(self,value):
+        self.__id = value
